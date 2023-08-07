3ee05cb06709a106764a6c0b7da7e72b987d8008
AshiqurRuhullah <ashiqurruhullah@gmail.com>
2023-08-08 00:03:45 +0600

    First commit

---
+++ AshiqFoodWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using AshiqFoodWeb.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace AshiqFoodWeb.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        protected ApplicationDbContext()
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
